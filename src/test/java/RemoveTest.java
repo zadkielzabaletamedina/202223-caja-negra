2a9f8804f3387914e4b7fe6ce8e6896df81bd975
Carlos Álvarez Camacho <carlos.alvarezc@alumnos.upm.es>
2022-11-30 21:34:57 +0100

    Clase RemoveTest

---
+++ src/test/java/RemoveTest.java
@@ -0,0 +1,67 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.binarytree.BinaryTree;
+import com.binarytree.Node;
+
+class RemoveTest {
+	BinaryTree arbolSoloRaiz;
+	BinaryTree arbolVariosNiveles;
+	BinaryTree arbolVariosNiveles1;
+	Node nodoNoEnElArbol;
+	Node nodoNull;
+	
+	@BeforeEach
+	void setUp() {
+		arbolSoloRaiz  = Util.crearArbolSoloRaiz();
+		arbolVariosNiveles = Util.crearArbolVariosNiveles();
+		arbolVariosNiveles1 = Util.crearArbolSoloHijoIzquierda();
+
+		nodoNoEnElArbol= new Node("6");
+		nodoNull= null;
+	}
+	
+	@Test
+	void variosNivelesNodoIntermedio() {
+		arbolVariosNiveles.remove(arbolVariosNiveles.getRoot().getLeftChild());
+		assertTrue(arbolVariosNiveles.getRoot().getLeftChild() == null);
+	}
+	@Test
+	void soloRaizNodoFueraArbol() {
+		assertThrows(Exception.class, () -> arbolSoloRaiz.remove(nodoNoEnElArbol));
+	}
+	@Test
+	void variosNivelesNodoFueraArbol() {
+		assertThrows(Exception.class, () -> arbolVariosNiveles.remove(nodoNoEnElArbol));
+	}
+	@Test
+	void soloRaizNodoNulo() {
+		assertThrows(Exception.class, () -> arbolSoloRaiz.remove(nodoNull));
+	}
+	@Test
+	void variosNivelesNodoNulo() {
+		assertThrows(Exception.class, () -> arbolVariosNiveles.remove(nodoNull));
+	}
+	@Test
+	void soloRaizNodoRaiz() {
+		arbolSoloRaiz.remove(arbolSoloRaiz.getRoot());
+		assertTrue(arbolSoloRaiz.getRoot() == null);
+		
+	}
+	@Test
+	void variosNivelesNodoRaiz() {
+		arbolVariosNiveles.remove(arbolVariosNiveles.getRoot());
+		assertTrue( arbolVariosNiveles.getRoot() == null);
+	}
+	@Test
+	void variosNivelesNodoHoja() {
+		arbolVariosNiveles.remove(arbolVariosNiveles.getRoot().getLeftChild().getLeftChild());
+		assertTrue( arbolVariosNiveles.getRoot().getLeftChild().getLeftChild() == null);
+	}
+	
+}
+
+
+
